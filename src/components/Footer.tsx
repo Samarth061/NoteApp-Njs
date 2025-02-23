f5d8217edda4371e942f21edbbd8e144b8ecb4d9
Samarth061 <140432072+Samarth061@users.noreply.github.com>
2025-02-23 00:04:32 -0600

    NoteApp 1.0 ready

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,54 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 export default function Footer() {
   return (
-    <footer className="w-full bg-gray-800 p-4 text-center text-white">
-      <p>
-        Layout Inspired by{" "}
-        <a
-          href="https://www.spithacode.com/blog/nextjs-15-deep-dive-building-notes-app-advanced-features"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-400 hover:underline"
-        >
-          Blog
-        </a>
-      </p>
+    <footer className="w-full bg-gray-800 p-4 text-center items-center text-white">
+      <div className="max-w-6xl mx-auto flex justify-between items-center">
+        <div className=" text-sm">
+          <p>
+            Created by <span className="font-semibold">Samarth Shah</span>
+          </p>
+        </div>
+        <div>
+          <p className="font-thin">
+            Layout Inspired by{" "}
+            <a
+              href="https://www.spithacode.com/blog/nextjs-15-deep-dive-building-notes-app-advanced-features"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-400 hover:underline"
+            >
+              Blog
+            </a>
+          </p>
+        </div>
+        {/* Contact Information */}
+        <div className="flex flex-row ">
+          <a
+            href="mailto:samarthdshah12@gmail.com"
+            className="text-blue-400 hover:underline"
+          >
+            Email
+          </a>
+          <span className="mx-2">|</span>
+          <a
+            href="https://github.com/Samarth061"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-400 hover:underline"
+          >
+            <FaGithub className="inline-block mr-1" /> GitHub
+          </a>
+          <span className="mx-2">|</span>
+          <a
+            href="https://www.linkedin.com/in/samarth-shah-319806224/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-400 hover:underline"
+          >
+            <FaLinkedin className="inline-block mr-1" /> LinkedIn
+          </a>
+        </div>
+      </div>
     </footer>
   );
 }
